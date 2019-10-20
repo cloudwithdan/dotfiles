53c6648a3af0748de4d7a81e3627d830c5f786f8
Daniel Nikoloski <nikoloskid@protonmail.com>
2019-10-20 18:31:31 +0000

    Create weather.sh

---
+++ .scripts/weather.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+#wttr.in — the right way to check the weather!
+#(c) Daniel Nikoloski (2019)
+
+echo
+  echo "USAGE: curl v2.wttr.in/CityName"
+echo
+exit 0;
+fi
+curl v2.wttr.in/Ljubljana
